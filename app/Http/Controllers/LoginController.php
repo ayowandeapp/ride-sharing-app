bafc866562ceb09e8d2f21cf16fde0af9aaf74f9
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2026-01-07 09:24:32 +0100

    initial commit, login and verify/login route, user login using login code via SMS notification

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\LoginNeedsVerification;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        //validate the phine number
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+        ]);
+
+        //find or create user model
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+
+        if (!$user) {
+            return response()->json(['message' => 'Could not process user with the phone number'], 401);
+        }
+
+        //send the user a one time use code
+        $user->notify(new LoginNeedsVerification());
+
+        return response()->json(['message' => 'Text message notification sent!'], 401);
+
+    }
+
+    public function verify(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999',
+        ]);
+
+        $user = User::where([
+            'phone' => $request->phone,
+            'login_code' => $request->login_code
+        ])->first();
+
+        if ($user) {
+            $user->update([
+                'login_code' => null
+            ]);
+            $token = $user->createToken($request->login_code);
+
+            return ['token' => $token->plainTextToken];
+        }
+
+        return response()->json(['message' => 'invalid verification code!'], 401);
+
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasApiTokens;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    //     'phone',
+    // ];
+    protected $guarded = [];
+
+    public function routeNotificationForTwilio()
+    {
+        return '+2349024472025';
+    }
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'login_code',
+        'password',
+        'remember_token',
+    ];
+
+    // /**
+    //  * Get the attributes that should be cast.
+    //  *
+    //  * @return array<string, string>
+    //  */
+    // protected function casts(): array
+    // {
+    //     return [
+    //         'email_verified_at' => 'datetime',
+    //         'password' => 'hashed',
+    //     ];
+    // }
+    public function driver(): HasOne
+    {
+        return $this->hasOne(Driver::class);
+    }
+
+    public function trips()
+    {
+        return $this->hasMany(Trip::class);
+    }
+}
