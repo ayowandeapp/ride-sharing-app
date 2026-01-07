c4e846f66c7cc77bddd9b0af23c3e08449d30387
Ayowande Oluwatosin <ayooluwa71@gmail.com>
2026-01-07 15:42:07 +0100

    Added events and broadcasting

--- app/Models/Trip.php
+++ app/Models/Trip.php
@@ -3,17 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property Driver|null $driver
+ * @property User|null $user
+ */
 class Trip extends Model
 {
     protected $guarded = [];
 
-    public function user()
+    protected $casts = [
+        'origin' => 'array',
+        'destination' => 'array',
+        'driver_location' => 'array',
+        'is_started' => 'boolean',
+        'is_complete' => 'boolean'
+    ];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function driver()
+    public function driver(): BelongsTo
     {
         return $this->belongsTo(Driver::class);
     }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\DriverController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TripController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +12,20 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::get('/driver', [DriverController::class, 'show']);
+    Route::post('/driver', [DriverController::class, 'update']);
+
+    Route::post('/trip', [TripController::class, 'store']);
+    Route::get('/trip/{trip}', [TripController::class, 'show']);
+    Route::post('/trip/{trip}/accept', [TripController::class, 'accept']);
+    Route::post('/trip/{trip}/start', [TripController::class, 'start']);
+    Route::post('/trip/{trip}/end', [TripController::class, 'end']);
+    Route::post('/trip/{trip}/location', [TripController::class, 'location']);
+
+
+
+});
+
